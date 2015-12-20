3bc632101254303f0a002a3aea87e35bbdd88dfb
Dylan McKay <dylan.mckay@powershop.co.nz>
2015-12-21 11:25:55 +1300

    Store the duration of each track

--- lib/track.rb
+++ lib/track.rb
@@ -1,13 +1,15 @@
 class Track
-  attr_reader :name, :track_number, :popularity
+  attr_reader :name, :track_number, :duration_seconds, :popularity
 
   def initialize(
     name,
     track_number:,
+    duration_seconds:,
     popularity:
   )
     @name = name
     @track_number = track_number
+    @duration_seconds = duration_seconds
     @popularity = popularity
   end
 
@@ -15,6 +17,7 @@ class Track
     Track.new(
       data['name'],
       track_number: data['track_number'],
+      duration_seconds: data['duration_ms'] / 1000.0,
       popularity: data['popularity'],
     )
   end
